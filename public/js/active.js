36800ad2060de719beb0ac74e53e8ac22d86c973
Иван <iozeryakov@gmail.com>
2022-05-21 21:08:35 +0300

    Диплом. JS

--- public/js/active.js
+++ public/js/active.js
@@ -1,14 +1,15 @@
 /**
- * Выделение активнной кнопки и вызов загрузки фильмов по данногму критерию
+ * Выделение активнной кнопки и вызов загрузки фильмов по данному критерию
  */
-import { getMovies } from './new.js';
+import { getMovies } from './index.js';
 const items = document.querySelectorAll('.selector__item');
 for( var i=0;i<items.length;i++){
     const link= items[i];
     link.addEventListener('click',function(){
-        const a=link.parentNode.querySelectorAll('.selector__item');
-        for (var j=0;j<a.length;j++){
-            a[j].classList.remove('selector__item_active');
+        const parent=link.parentNode;
+        for (var j=0;j<items.length;j++){
+            if(items[j].parentNode===parent)
+                items[j].classList.remove('selector__item_active');
         }
         this.classList.add('selector__item_active');
         getMovies(this.id,'ru');
